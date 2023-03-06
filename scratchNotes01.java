faf0aa1d5a4c3913030ecead74387236413e9c67
diamondmuffin01 <80565512+diamondmuffin01@users.noreply.github.com>
2023-03-06 15:38:51 -0800

    Update scratchNotes01.java

--- scratchNotes01.java
+++ scratchNotes01.java
@@ -35,8 +35,52 @@ public class Socket_Test
 
 
 
-
 /*______________________________________________________________*/
+
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class SocketClient
+{
+    public String connectForOneMessage(String sIP, int iPort, String sMessage)
+    {
+        try(Socket oSocket = new Socket())
+        {
+            //attempt to connect to server.
+            oSocket.connect(new InetSocketAddress(sIP, iPort), 5000);
+
+            //preparing for output.
+            OutputStream output = oSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            //send message
+            writer.println(sMessage);
+            writer.flush();
+
+            //get reply from server
+            InputStream input = oSocket.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+            //???
+            String sReturn = reader.readLine();
+
+            oSocket.close();
+
+            return sReturn;
+        }
+        catch(Exception ex)
+        {
+            System.out.println("[client]: Client exception: " + ex.getMessage());
+            ex.printStackTrace();
+            return null;
+        }
+
+    }
+}
+
+/*________________________________________________________________________*/
 
 
 import java.io.IOException;
@@ -92,51 +136,6 @@ public class SocketServer implements Runnable
     }
 }
 
-
-/*________________________________________________________________________*/
-
-
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-
-public class SocketClient
-{
-    public String connectForOneMessage(String sIP, int iPort, String sMessage)
-    {
-        try(Socket oSocket = new Socket())
-        {
-            //attempt to connect to server.
-            oSocket.connect(new InetSocketAddress(sIP, iPort), 5000);
-
-            //preparing for output.
-            OutputStream output = oSocket.getOutputStream();
-            PrintWriter writer = new PrintWriter(output, true);
-
-            //send message
-            writer.println(sMessage);
-            writer.flush();
-
-            //get reply from server
-            InputStream input = oSocket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-
-            //???
-            String sReturn = reader.readLine();
-
-            oSocket.close();
-
-            return sReturn;
-        }
-        catch(Exception ex)
-        {
-            System.out.println("[client]: Client exception: " + ex.getMessage());
-            ex.printStackTrace();
-            return null;
-        }
-
-    }
-}
 
 
 
