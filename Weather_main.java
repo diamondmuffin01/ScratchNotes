acbb998263e2df90b8250ac64a58151200b18f6f
diamondmuffin01 <80565512+diamondmuffin01@users.noreply.github.com>
2023-05-08 11:06:20 -0700

    Add files via upload

---
+++ Weather_main.java
@@ -0,0 +1,15 @@
+package com.grossmont.ws;
+
+
+public class Weather_main {
+
+    public float temp;
+    public float pressure;
+    public float humidity;
+    public float temp_min;
+    public float temp_max
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
